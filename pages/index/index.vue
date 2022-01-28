f45c0bfda547877e4ea6f1098b5e9110282874f4
Finn <germxu@gamil.com>
2022-01-28 23:27:06 +0800

    add prettier

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,41 +1,43 @@
 <template lang="pug">
-view(class="content")
-	image(class="logo" src="/static/logo.png")
-	view(class="text-area")
-		text(class="title") {{title}}
-
+view.content
+  image.logo(src="/static/logo.png")
+  view.text-area
+    text.title {{ title }}
 </template>
 
 <script>
 export default {
   setup() {
+    onShow: () => {
+      console.log("222")
+    }
     return {
       title: "Hello uniapp_v3",
-    };
+    }
   },
-};
+}
 </script>
 
 <style lang="sass">
 .content
-	display: flex
-	flex-direction: column
-	align-items: center
-	justify-content: center
+  display: flex
+  flex-direction: column
+  align-items: center
+  justify-content: center
 
 .logo
-	height: 200rpx
-	width: 200rpx
-	margin-top: 200rpx
-	margin-left: auto
-	margin-right: auto
-	margin-bottom: 50rpx
+  height: 200rpx
+  width: 200rpx
+  margin-top: 200rpx
+  margin-left: auto
+  margin-right: auto
+  margin-bottom: 50rpx
 
 .text-area
-	display: flex
-	justify-content: center
+  display: flex
+  justify-content: center
 
 .title
-	font-size: 36rpx
-	color: #8f8f94
+  font-size: 36rpx
+  color: #8f8f94
 </style>
